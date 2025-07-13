caaae270b4269deec0b05b123a9ffb0dfff7c08c
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-07-13 09:06:04 +0530

    change layout

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -31,38 +31,22 @@ export const projects: Project[] = [
     tags: ['Leptos', 'Rust']
   },
   {
-    ...(await getRepositoryDetails('devaradise/paradise-ui')),
     name: 'Go-BookStore-API',
     description: 'To create book API operation using Golang',
     demoLink: 'https://github.com/dev-dhanushkumar/Go-BookStore-API',
-    // postLink: 'https://devaradise.com/how-i-build-paradise-ui-react-component-library/',
     tags: ['Golang', 'REST API']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/react-lab')),
     name: 'JS-WebRTC-Video-Chat',
     description: 'JS WebRTC Video Chat App',
     demoLink: 'https://github.com/dev-dhanushkumar/JS-Small-Projects/tree/main/js-WebRTC-Video-Chat',
     tags: ['Javascript']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/pokemon-catcher')),
     name: 'Golang-ToDo-List-CLI-Tool',
     description:'command-line tool designed to help you manage your daily tasks efficiently',
     demoLink: 'https://github.com/dev-dhanushkumar/Golang-Projects/tree/main/golang_task',
     postLink: 'https://dev.to/dev-dhanushkumar/mytask-todo-cli-tool-2kej',
     tags: ['CLI', 'Golang']
   },
-  // {
-  //   ...(await getRepositoryDetails('syakirurahman/movie-nominations')),
-  //   name: 'Movie Nominations',
-  //   demoLink: 'https://movie-nominations-c21c3.web.app/',
-  //   tags: ['Hobby']
-  // },
-  // {
-  //   ...(await getRepositoryDetails('syakirurahman/organization-tree')),
-  //   name: 'Organization tree',
-  //   demoLink: 'https://organization-tree-2a446.web.app/',
-  //   tags: ['Hobby']
-  // }
 ]
